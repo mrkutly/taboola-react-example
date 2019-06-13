650c6d32007bfb91043a14f065045e42bcb6dc32
mark.sauer.utley <mark.sauer.utley@taboola.com>
2019-06-13 15:05:00 -0400

    refactored taboola plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,6 +25,23 @@ const Layout = ({ children }) => (
         `}
         render={data => (
             <>
+                <head>
+                    <script type="text/javascript">
+                        {(function(e, f, u, i) {
+                            if (!document.getElementById(i)) {
+                                e.async = 1
+                                e.src = u
+                                e.id = i
+                                f.parentNode.insertBefore(e, f)
+                            }
+                        })(
+                            document.createElement('script'),
+                            document.getElementsByTagName('script')[0],
+                            '//cdn.taboola.com/libtrc/taboola-training/loader.js',
+                            'tb_loader_script'
+                        )}
+                    </script>
+                </head>
                 <Header siteTitle={data.site.siteMetadata.title} />
                 <div
                     style={{
